adb61fea27c8e698ad463777ac1d24dbb09aa130
pavigodj <saturnturn@yahoo.com>
2024-07-09 03:02:18 -0700

    cross browser driver support added

--- ecommerce/conftest.py
+++ ecommerce/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -10,46 +11,28 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="class")
 def driverSetUp(request):
+    # Setup
     browserName = request.config.getoption("browser")
-    print(browserName)
     if browserName == "firefox":
         options_obj = webdriver.FirefoxOptions()
         #certfication error handling, maximizing screen, making browser headless
         options_obj.add_argument("--ignore-certification-errors")
-        service_obj = Service("/Users/bavi/Downloads/geckodriver 3")
+        service_obj = FirefoxService("/Users/bavi/Downloads/geckodriver 3")
         driver = webdriver.Firefox(service=service_obj, options=options_obj)
+    elif browserName == "edge":
+        options_obj = webdriver.EdgeOptions()
+        options_obj.add_argument("--ignore-certificate-errors")
+        service_obj = EdgeService("/Users/bavi/Downloads/msedgedriver")
+        driver = webdriver.Edge(service=service_obj, options=options_obj)        
     elif browserName == "chrome":
-        options_obj = Options()
-        options_obj.add_argument('--headless=new')
-        #certfication error handling, maximizing screen, making browser headless
-        options_obj.add_argument("--ignore-certification-errors")
-        driver = webdriver.Chrome(options=options_obj)
-    # driver.get("https://opencart.abstracta.us/")
+        chromedriver_path = "/Users/bavi/Downloads/chromedriver"
+        service = ChromeService(chromedriver_path)
+        options_obj = webdriver.ChromeOptions()
+        options_obj.add_argument("--ignore-certificate-errors")
+        driver = webdriver.Chrome(service=service, options=options_obj)
     if request.cls:  # Skip for testcase which is not a class
         request.cls.driver = driver
     driver.maximize_window()
     yield driver
+    # Cleanup
     driver.quit()
-
-@pytest.fixture(scope="class")
-def driverSetUp_2(request):
-    browserName = request.config.getoption("browser")
-    print(browserName)
-    if browserName == "firefox":
-        options_obj = webdriver.FirefoxOptions()
-        #certfication error handling, maximizing screen, making browser headless
-        options_obj.add_argument("--ignore-certification-errors")
-        service_obj = Service("/Users/bavi/Downloads/geckodriver 3")
-        driver = webdriver.Firefox(service=service_obj, options=options_obj)
-    elif browserName == "chrome":
-        options_obj = Options()
-        options_obj.add_argument('--headless=new')
-        #certfication error handling, maximizing screen, making browser headless
-        options_obj.add_argument("--ignore-certification-errors")
-        driver = webdriver.Chrome(options=options_obj)                                  
-    request.cls.driver = driver
-    # driver.get("https://opencart.abstracta.us/")
-    driver.maximize_window()
-    yield driver
-    driver.quit()
-
